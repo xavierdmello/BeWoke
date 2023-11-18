db77fd9eb463f6fc0001776d629f42b7dde3f18b
Xavier DMello <xaviermdmello@gmail.com>
2023-11-18 17:33:38 -0500

    dsad

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,13 @@ function App() {
     const [result, setResult] = useState<string>("");
     const [capturedImage, setCapturedImage] = useState<string | null>(null);
     const [task, setTask] = useState<string>("");
-    const [menu, setMenu] = useState<string>("takebewoke");
+    const [menu, setMenu] = useState<string>("setalarm");
+    const [alarmTime, setAlarmTime] = useState<string>("");
 
   return <Flex flexDirection={"column"} backgroundColor={"black"} height={"100vh"} mx="auto" alignItems={"center"} maxWidth={"700px"} width={"100%"} borderX={"1px solid gray "}>
     <Header/>
       {menu == "takebewoke" && <TakeBeWoke base64={base64} setBase64={setBase64} result={result} setResult={setResult} capturedImage={capturedImage} setCapturedImage={setCapturedImage} task={task} setTask={setTask}/>}
-      {menu == "setalarm" && <SetAlarm/>}
+      {menu == "setalarm" && <SetAlarm task={task} setTask={setTask} alarmTime={alarmTime} setAlarmTime={setAlarmTime}/>}
   </Flex>;
 }
 
--- src/SetAlarm.tsx
+++ src/SetAlarm.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Image, Flex, Input, Heading } from "@chakra-ui/react";
 
-const SetAlarm: React.FC = () => {
-  return (
-    <Box bg="black">
 
-    </Box>
-  );
+interface SetAlarmProps {
+    task: string;
+    setTask: (task: string) => void;
+}
+
+interface SetAlarmProps {
+    task: string;
+    setTask: (task: string) => void;
+    alarmTime: string;
+    setAlarmTime: (time: string) => void;
+}
+
+const SetAlarm: React.FC<SetAlarmProps> = ({ task, setTask, alarmTime, setAlarmTime }) => {
+    return (
+      <Flex flexDirection="column" alignItems="center">
+        <Heading fontSize="2xl" color="white" mt="10px">
+          In the morning, I will:
+        </Heading>
+        <Input value={task} onChange={(e) => setTask(e.target.value)} placeholder="Morning Task" color="white"  mt="10px" mb="10px" />
+
+        <Heading fontSize="2xl" color="white" mt="10px">
+          Alarm Time
+        </Heading>
+        <Input value={alarmTime} onChange={(e) => setAlarmTime(e.target.value)} placeholder="Alarm Time" color="white" mt="10px" mb="10px" />
+      </Flex>
+    );
 };
 
 export default SetAlarm;
+
