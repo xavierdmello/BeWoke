82e0c87ae4b7f21ce679890249c59110877eea89
Xavier DMello <xaviermdmello@gmail.com>
2023-11-18 13:11:25 -0500

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const OPENAI_KEY = import.meta.env.VITE_OPENAI_KEY;
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: OPENAI_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+async function chat(input: string) {
+  const promptTemplate = `Be very funny when answering questions.
+
+  Question: ${input}`;
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: `${promptTemplate}` }],
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+  });
+
+  return chatCompletion;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    chat("What's the capital of france?").then((res) => {
+      setMessage(res.choices[0].message.content!);
+    });
+
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{message}</h1>  
     </>
-  )
+  );
 }
 
-export default App
+export default App;
